756cd54cc720a105255f227353376fcce3f66d59
Neloxe <adrienvrh@proton.me>
2024-05-30 11:46:04 +0200

    Azure files service

--- src/fileazure/filesAzure.service.ts
+++ src/fileazure/filesAzure.service.ts
@@ -1,12 +1,14 @@
 import { BlobServiceClient, BlockBlobClient } from '@azure/storage-blob';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import sharp from 'sharp';
 import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class FilesAzureService {
   constructor(private readonly configService: ConfigService) {}
   private containerName: string;
+  private authorizeExtension = ['jpg', 'jpeg', 'png', 'webp'];
 
   private async getBlobServiceInstance() {
     const connectionString = this.configService.get('CONNECTION_STRING');
@@ -28,11 +30,35 @@ export class FilesAzureService {
   public async uploadFile(file: Express.Multer.File, containerName: string) {
     this.containerName = containerName;
     const extension = file.originalname.split('.').pop();
+    if (this.authorizeExtension.includes(extension) === false) {
+      throw new BadRequestException('Invalid file extension');
+    }
     const file_name = uuidv4() + '.' + extension;
-    console.log(file_name);
+    const compressedFilePath = `compressed-${file_name}`;
+
+    let quality = 80;
+    let buffer = file.buffer;
+    let info;
+
+    do {
+      const result = await sharp(buffer)
+        .resize({ width: 800, withoutEnlargement: true })
+        .jpeg({ quality })
+        .toBuffer({ resolveWithObject: true })
+        .then((data) => {
+          console.log(data);
+          return data;
+        });
+
+      buffer = result.data;
+      info = result.info;
+
+      quality -= 10;
+    } while (buffer.length / 1024 > 100 && quality > 10);
+
     const blockBlobClient = await this.getBlobClient(file_name);
     const fileUrl = blockBlobClient.url;
-    await blockBlobClient.uploadData(file.buffer);
+    await blockBlobClient.uploadData(buffer);
 
     return fileUrl;
   }
